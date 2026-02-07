5f80f3ef5ba923b714676f8629e770705d38c156
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-02-07 18:22:29 +0530

    feat: add hover effect on group

--- src/components/section/hackathons-section.tsx
+++ src/components/section/hackathons-section.tsx
@@ -20,14 +20,14 @@ export default function HackathonsSection() {
           <div className="flex flex-col gap-y-3 items-center justify-center">
             {/* <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl">I like building things</h2> */}
             <p className="text-muted-foreground md:text-lg/relaxed lg:text-base/relaxed xl:text-lg/relaxed text-balance text-center">
-            While Learning i've thought to contribute to Open Source Projects which eventually taught me a lot
-            Currently i've contributed to {DATA.hackathons.length} projects and thinking to contribute more
+              While Learning i've thought to contribute to Open Source Projects which eventually taught me a lot
+              Currently i've contributed to {DATA.hackathons.length} projects and thinking to contribute more
             </p>
           </div>
         </div>
         <Timeline>
           {DATA.hackathons.map((hackathon) => (
-            <TimelineItem key={hackathon.title + hackathon.dates} className="w-full flex items-start justify-between gap-10">
+            <TimelineItem key={hackathon.title + hackathon.dates} className="w-full flex items-start justify-between gap-10 group/hackathon">
               <TimelineConnectItem className="flex items-start justify-center">
                 {hackathon.image ? (
                   <img
@@ -46,12 +46,12 @@ export default function HackathonsSection() {
                 {hackathon.title && (
                   <a href={hackathon.website} target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 group">
                     <h3 className="font-semibold leading-none">{hackathon.title}</h3>
-                    <ArrowUpRight className="h-3.5 w-3.5 text-muted-foreground opacity-0 -translate-x-2 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-200" aria-hidden />
+                    <ArrowUpRight className="h-3.5 w-3.5 text-muted-foreground opacity-0 -translate-x-2 group-hover/hackathon:opacity-100 group-hover/hackathon:translate-x-0 transition-all duration-200" aria-hidden />
                   </a>
                 )}
                 {hackathon.repository && (
-                  <a href={hackathon.repoUrl} target="_blank" rel="noopener noreferrer"   className="flex items-center gap-2 group hover:underline">
-                    <p className="text-sm text-muted-foreground">{hackathon.repository}</p>
+                  <a href={hackathon.repoUrl} target="_blank" rel="noopener noreferrer" className="flex items-center gap-2">
+                    <p className="text-sm text-muted-foreground relative inline after:content-[''] after:absolute after:h-[1px] after:w-full after:scale-x-0 after:bg-current after:bottom-0 after:left-0 group-hover/hackathon:after:scale-x-100 after:transition-transform after:duration-500 after:ease-out after:origin-left hover:no-underline">{hackathon.repository}</p>
                   </a>
                 )}
                 {hackathon.description && (
